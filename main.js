829d4504c979a159e4b8d8c760509e182cd143d0
Tommy Martinez <tommy@Tommys-MacBook-Pro.local>
2024-05-06 19:20:47 -0400

    test setup

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const body = document.querySelector('body');
   const title = document.createElement('h1');
-
   title.innerHTML = 'HELLO PALOMA';
-
   body.appendChild(title);
 });
